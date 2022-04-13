0f3ae7dd8ef7eacde50dd38c24bd4ed64a1b7117
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-04-13 12:09:53 +0300

    Next step to getting inf from getted and saved information

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+# 1. Getting information from website
 # url = "https://health-diet.ru/table_calorie/"
 # url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 #
 # headers = {
 #       "accept": "*/*",
-#       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
+#       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)" \
+#                     " Chrome/99.0.4844.74 Safari/537.36",
 # }
 # req = requests.get(url, headers=headers)
 # src = req.text
@@ -16,24 +18,26 @@ import json
 # with open("index.html", "w") as file:
 #     file.write(src)
 
-with open("index.html") as file:
-    src = file.read()
+# with open("index.html") as file:
+#     src = file.read()
+#
+# soup = BeautifulSoup(src, 'html.parser')
+# # print(soup)
+#
+# all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+#
+# all_categories_dict = {}
+# for item in all_products_hrefs:
+#     item_text = item.text
+#     item_href = "https://health-diet.ru" + item.get("href")
+#     # print(f"{item_text}:{item_href}")
+#     all_categories_dict[item_text] = item_href
+#
+# with open("all_categories_dict.json", "w") as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
 
-soup = BeautifulSoup(src, 'html.parser')
-# print(soup)
+with open("all_categories_dict.json") as file:
+    all_categories = json.load(file)
+# print(all_categories)
 
-all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
-
-all_categories_dict = {}
-for item in all_products_hrefs:
-    item_text = item.text
-    item_href = "https://health-diet.ru" + item.get("href")
-    # print(f"{item_text}:{item_href}")
-    all_categories_dict[item_text] = item_href
-
-with open("all_categories_dict.json", "w") as file:
-    json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
-
-
-
-# print(all_categories_dict)
+# 2. Working with getted and saved information
