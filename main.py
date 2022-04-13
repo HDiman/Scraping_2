7d1bfa29baef0310b52c036699d5311264e4c816
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-04-13 11:44:31 +0300

    Creating dictionary from new information

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ soup = BeautifulSoup(src, 'html.parser')
 
 all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
 
+all_categories_dict = {}
 for item in all_products_hrefs:
     item_text = item.text
-    item_href = item.get("href")
-    print(f"{item_text}:{item_href}")
+    item_href = "https://health-diet.ru" + item.get("href")
+    # print(f"{item_text}:{item_href}")
+    all_categories_dict[item_text] = item_href
+
+print(all_categories_dict)
