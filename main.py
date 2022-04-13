aeed2678691d3953679fcdd14829537b8c9acf32
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-04-13 11:35:53 +0300

    Getting inf from index.html

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 # url = "https://health-diet.ru/table_calorie/"
-url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+# url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+#
+# headers = {
+#       "accept": "*/*",
+#       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
+# }
+# req = requests.get(url, headers=headers)
+# src = req.text
+# # print(src)
+#
+# with open("index.html", "w") as file:
+#     file.write(src)
 
-headers = {
-      "accept": "*/*",
-      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
-}
-req = requests.get(url, headers=headers)
-src = req.text
-# print(src)
+with open("index.html") as file:
+    src = file.read()
 
-with open("index.html", "w") as file:
-    file.write(src)
+soup = BeautifulSoup(src, 'html.parser')
+# print(soup)
 
+all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
 
-
-
-# soup = BeautifulSoup(url, 'html.parser')
-
+for item in all_products_hrefs:
+    item_text = item.text
+    item_href = item.get("href")
+    print(f"{item_text}:{item_href}")
