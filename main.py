19b1249c09bd0ec83dc7a76a67f20ca22abbc939
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-04-13 10:45:38 +0300

    Saving website to local host for further work

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Let's start")
+import requests
+from bs4 import BeautifulSoup
+
+# url = "https://health-diet.ru/table_calorie/"
+url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+
+headers = {
+      "accept": "*/*",
+      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
+}
+req = requests.get(url, headers=headers)
+src = req.text
+# print(src)
+
+with open("index.html", "w") as file:
+    file.write(src)
+
+
+
+
+# soup = BeautifulSoup(url, 'html.parser')
+
